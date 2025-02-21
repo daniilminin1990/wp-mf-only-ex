3e50c0d0754ac4960081eab4bfc0d0aa0f98f99b
Daniil <daniilminin1990@yandex.ru>
2025-02-22 01:25:19 +0500

    add more preparations before module federation

---
+++ services/host/src/bootstrap.tsx
@@ -0,0 +1,17 @@
+import {createRoot} from "react-dom/client";
+import {RouterProvider} from "react-router-dom";
+import {router} from "@/reouter/Router";
+
+const root = document.getElementById('root')
+
+if (!root){
+  throw new Error('Root element not found')
+}
+
+const container = createRoot(root)
+
+
+// container.render(<App />)
+container.render(
+  <RouterProvider router={router} />
+)
