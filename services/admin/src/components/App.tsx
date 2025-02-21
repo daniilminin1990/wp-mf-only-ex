27754d379c77762613860d5e9274a3e425688aec
Daniil <daniilminin1990@yandex.ru>
2025-02-21 23:31:19 +0500

    prepared to add module federation

---
+++ services/admin/src/components/App.tsx
@@ -0,0 +1,14 @@
+import {Link, Outlet} from "react-router-dom";
+
+export const App = () => {
+  return (
+    <div data-testId = {'App'}>
+      <h1>PAGE</h1>
+      <Link to={'/about'}>ABOUT</Link>
+      <br/>
+      <Link to={'/shop'}>SHOP</Link>
+      <br/>
+      <Outlet />
+    </div>
+  );
+};
--- services/admin/src/index.tsx
+++ services/admin/src/index.tsx
@@ -2,6 +2,9 @@ import {createRoot} from "react-dom/client";
 // import {App} from "../components/App";
 import {App} from "@/components/App";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {LazyAbout} from "@/pages/about/About.lazy";
+import {Shop} from "@/pages/shop";
+import {Suspense} from "react";
 
 const root = document.getElementById('root')
 
@@ -16,15 +19,15 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
-      // {
-      //   path: '/about',
-      //   element: <Suspense fallback={<h1>Loading about...</h1>}><LazyAbout /></Suspense>,
-      // },
-      // // Чуть позже вместо компонент тут будут микрофронты, целый магазин
-      // {
-      //   path: '/shop',
-      //   element: <Suspense fallback={<h1>Loading shop...</h1>}><Shop /></Suspense>
-      // },
+      {
+        path: '/about',
+        element: <Suspense fallback={<h1>Loading about...</h1>}><LazyAbout /></Suspense>,
+      },
+      // Чуть позже вместо компонент тут будут микрофронты, целый магазин
+      {
+        path: '/shop',
+        element: <Suspense fallback={<h1>Loading shop...</h1>}><Shop /></Suspense>
+      },
     ]
   }
 ])
